7e16d25f11ed5cc3b21e209a1db40da9bc1a7a1a
tielefernandes <tielefernandes@gmail.com>
2023-09-15 15:05:58 -0300

    :zap: test [aula-4]: melhorando a funcionalidade de login

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,6 +1,7 @@
 Cypress.Commands.add('login', (
     user = Cypress.env('user_name'),
     password = Cypress.env('user_password'),
+    { cacheSession = true } = {},
 ) => {
     const login = () => {
         cy.visit('/users/sign_in')
@@ -10,10 +11,34 @@ Cypress.Commands.add('login', (
         cy.get("[data-qa-selector='sign_in_button']").click()
     }
 
-    login()
+    const validate = () => {
+        cy.visit('/')
+        cy.location('pathname', { timeout: 1000 })
+            .should('not.eq', '/users/sign_in')
+    }
+
+    const options = {
+        cacheAcrossSpecs: true,
+        validate,
+    }
+
+    if (cacheSession) {
+        cy.session(user, login, options)
+    } else {
+        login()
+    }
 })
 
 Cypress.Commands.add('logout', () => {
     cy.get('.qa-user-avatar').click()
     cy.contains('Sign out').click()
+})
+
+Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('/projects/new')
+
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get('.qa-initialize-with-readme-checkbox').check()
+    cy.contains('Create project').click()
 })
