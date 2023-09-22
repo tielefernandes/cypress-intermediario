ff40031fe1acac86a5709c95b4ce1c7662f5d7de
tielefernandes <tielefernandes@gmail.com>
2023-09-22 15:35:56 -0300

    :test_tube: test: Testando criação de issue via API

--- cypress/e2e/api/createIssue.cy.js
+++ cypress/e2e/api/createIssue.cy.js
@@ -1,19 +1,23 @@
 import { faker } from '@faker-js/faker'
 
-describe('Create Project', () => {
-    beforeEach(() => cy.api_deleteProjects())
-    
-    it('successfully', () => {
-        const project = {
-            name: `project-${faker.datatype.uuid()}`,
-            description: faker.random.words(5)
-        }
+describe('Create issue', () => {
+  beforeEach(() => cy.api_deleteProjects())
 
-        cy.api_createProject(project)
-            .then(response => {
-                expect(response.status).to.equal(201)
-                expect(response.body.name).to.equal(project.name)
-                expect(response.body.description).to.equal(project.description)
-            })
-    })
+  it('successfully', () => {
+    const issue = {
+      title: `issue-${faker.datatype.uuid()}`,
+      description: faker.random.words(3),
+      project: {
+        name: `project-${faker.datatype.uuid()}`,
+        description: faker.random.words(5)
+      }
+    }
+
+    cy.api_createIssue(issue)
+      .then(response => {
+        expect(response.status).to.equal(201)
+        expect(response.body.title).to.equal(issue.title)
+        expect(response.body.description).to.equal(issue.description)
+      })
+  })
 })
